0dda0f5c12914c0661a0f17dc60da860ced29cba
allagan-node <allagan.node@gmail.com>
2020-01-09 22:29:04 +0000

    versioning

--- AllaganNode/MainWindow.xaml.cs
+++ AllaganNode/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,8 @@ namespace AllaganNode
         public MainWindow()
         {
             InitializeComponent();
+
+            TitleTextBlock.Text = "Allagan Node " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
     }
 }
